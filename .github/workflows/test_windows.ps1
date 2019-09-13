a2d045100aa4d6897f1ce48b0d59e442bab1d1ff
Shivam Mathur <shivam_jpr@hotmail.com>
2019-09-13 15:32:37 +0530

    init darwin

--- .github/workflows/test_windows.ps1
+++
@@ -1,2 +0,0 @@
-Move-item -Path C:\ProgramData\ComposerBin\composer.bat -Destination c:\tools\php\composer.bat -Force
-Move-item -Path C:\ProgramData\ComposerBin\composer.phar -Destination c:\tools\php\composer.phar -Force
