2a0c957dd2df11b27eedd79d43b8b4a177fc7269
Shivam Mathur <shivam_jpr@hotmail.com>
2019-10-13 13:03:38 +0530

    Fix permissions

--- .github/workflows/phalcon.sh
+++ .github/workflows/phalcon.sh
@@ -14,8 +14,8 @@ else
   git clone --depth=1 -v https://github.com/phalcon/cphalcon.git -b "$1"
   (
     cd cphalcon/build/php7/64bits && /usr/bin/phpize"$2"
-    ./configure --silent --with-php-config=/usr/bin/php-config"$2" --enable-phalcon
-    make -j6 && make install
+    sudo ./configure --silent --with-php-config=/usr/bin/php-config"$2" --enable-phalcon
+    sudo make -j6 && sudo make install
     echo "extension=phalcon.so" >> "$ini_file"
   )
 fi
