403d8c56477ebc617de02b1eb4bf8135eec301df
Shivam Mathur <shivam_jpr@hotmail.com>
2019-10-13 11:56:27 +0530

    Set correct tools

--- .github/workflows/phalcon.sh
+++ .github/workflows/phalcon.sh
@@ -1,5 +1,10 @@
 ini_file=$(php --ini | grep "Loaded Configuration" | sed -e "s|.*:s*||" | sed "s/ //g")
 sudo DEBIAN_FRONTEND=noninteractive apt install php"$2"-dev php-pear -y
+for tool in php-config phpize; do
+  if [ -e "/usr/bin/$tool$2" ]; then
+    sudo update-alternatives --set $tool /usr/bin/"$tool$2" &
+  fi
+done
 sudo pecl config-set php_ini "$ini_file"
 sudo pear config-set php_ini "$ini_file"
 sudo pecl install psr
