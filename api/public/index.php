1deb7e2bc5b87115d8580931da05155d81b5473c
Shivam Mathur <shivam_jpr@hotmail.com>
2019-10-13 16:52:31 +0530

    load dotenv

--- api/public/index.php
+++ api/public/index.php
@@ -4,6 +4,9 @@ use Niden\Bootstrap\Api;
 
 require_once __DIR__ . '/../../library/Core/autoload.php';
 
+$dotenv = Dotenv\Dotenv::create(__DIR__);
+$dotenv->load();
+
 $bootstrap = new Api();
 
 $bootstrap->setup();
