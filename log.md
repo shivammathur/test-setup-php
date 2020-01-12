de23b407450ad913f024c8415e0d314320a7df7f
Shivam Mathur <shivam_jpr@hotmail.com>
2020-01-13 01:32:35 +0530

    test issue 145

--- log.md
+++
@@ -1,9 +0,0 @@
-# Test Log (Created: 2019-11-30, 1.5.6 onwards)
-- Test on schedule.
-- Update schedule to every 6 hrs.
-- Test 1.5.7 release
-- Test 1.6.0 release
-- Test 1.7.1 release
-- Test 1.7.1 release
-- Test 1.7.1 release
-- Test 1.7.2 release
---
+++ src/Commands/DisableCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Nwidart\Modules\Commands;
+
+use Illuminate\Console\Command;
+use Nwidart\Modules\Module;
+use Symfony\Component\Console\Input\InputArgument;
+
+class DisableCommand extends Command
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'module:disable';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Disable the specified module.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        /** @var Module $module */
+        $module = $this->laravel['modules']->findOrFail($this->argument('module'));
+
+        if ($module->isEnabled()) {
+            $module->disable();
+
+            $this->info("Module [{$module}] disabled successful.");
+        } else {
+            $this->comment("Module [{$module}] has already disabled.");
+        }
+    }
+
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return [
+            ['module', InputArgument::REQUIRED, 'Module name.'],
+        ];
+    }
+}
