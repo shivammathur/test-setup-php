92c956a0d56dd0b411be7a5650ffa8ecb203427f
Kevin Batdorf <kbat82@gmail.com>
2020-12-07 23:40:04 +0700

    wip

--- .github/actions/build-wp-container/entrypoint.sh
+++ .github/actions/build-wp-container/entrypoint.sh
@@ -7,7 +7,7 @@ pwd
 wp --info
 ls
 
-wp config create --dbname=wordpress --dbuser=wordpress --dbpass=wordpress --dbhost=mysql --allow-root
+wp config create --dbname=wordpress --dbuser=wordpress --dbpass=wordpress --dbhost=mysql --allow-root --debug
 wp core install --url=http://localhost:8080 --title=Test --admin_user=wordpress --admin_password=wordpress --admin_email=admin@local.test --skip-email --allow-root
 # wp plugin install woocommerce --activate --allow-root
 
