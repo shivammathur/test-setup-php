e8c66e82f84c09b19bf1fdd559d3b636f8bd6c71
Shivam Mathur <shivam_jpr@hotmail.com>
2020-12-30 02:20:53 +0530

    testing

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -11,6 +11,7 @@ setup_phpbuild() {
     echo "--enable-cgi" | sudo tee -a /usr/local/share/php-build/default_configure_options >/dev/null 2>&1
   elif [ "$TYPE" = "fpm" ]; then
     sudo sed -i "/cgi/d" /usr/local/share/php-build/default_configure_options
+    echo '"--with-apxs2" "/usr/bin/apxs2"' | sudo tee -a /usr/local/share/php-build/default_configure_options >/dev/null 2>&1
     echo "--enable-fpm" | sudo tee -a /usr/local/share/php-build/default_configure_options >/dev/null 2>&1
     echo "--with-fpm-user=www-data" | sudo tee -a /usr/local/share/php-build/default_configure_options >/dev/null 2>&1
     echo "--with-fpm-group=www-data" | sudo tee -a /usr/local/share/php-build/default_configure_options >/dev/null 2>&1
@@ -18,6 +19,7 @@ setup_phpbuild() {
 }
 
 setup_pear() {
+  sudo rm -rf "$install_dir"/bin/pear "$install_dir"/bin/pecl
   sudo curl -fsSL --retry "$tries" -o /usr/local/ssl/cert.pem https://curl.haxx.se/ca/cacert.pem
   sudo curl -fsSL --retry "$tries" -O https://github.com/pear/pearweb_phars/raw/v1.9.5/go-pear.phar
   sudo chmod a+x .github/scripts/install-pear.expect
@@ -30,14 +32,12 @@ setup_pear() {
 configure_php_fpm() {
   sudo ln -sv "$install_dir"/sbin/php-fpm "$install_dir"/bin/php-fpm
   sudo mkdir -p "$install_dir"/etc/systemd/system
-  sudo sed -Ei "s|^listen = .*|listen = /run/php/php$PHP_VERSION-fpm.sock|" "$install_dir"/etc/php-fpm.d/www.conf
+  sudo sed -Ei "s|^listen = .*|listen = /run/php/php$PHP_VERSION-fpm.sock|" "$install_dir"/etc/php-fpm.conf
   sudo sed -Ei 's|;listen.owner.*|listen.owner = www-data|' "$install_dir"/etc/php-fpm.conf
   sudo sed -Ei 's|;listen.group.*|listen.group = www-data|' "$install_dir"/etc/php-fpm.conf
   sudo sed -Ei 's|;listen.mode.*|listen.mode = 0660|' "$install_dir"/etc/php-fpm.conf
   sudo sed -Ei "s|;pid.*|pid = /run/php/php$PHP_VERSION-fpm.pid|" "$install_dir"/etc/php-fpm.conf
   sudo sed -Ei "s|;error_log.*|error_log = /var/log/php$PHP_VERSION-fpm.log|" "$install_dir"/etc/php-fpm.conf
-  sudo sed -i "s/PHP_VERSION/$PHP_VERSION/g" .github/scripts/fpm.service
-  sudo sed -i "s/NO_DOT/${PHP_VERSION/./}/g" .github/scripts/fpm.service
   sudo cp -fp .github/scripts/fpm.service "$install_dir"/etc/systemd/system/php-fpm.service
   sudo cp -fp .github/scripts/php-fpm-socket-helper "$install_dir"/bin/
   sudo chmod a+x "$install_dir"/bin/php-fpm-socket-helper
