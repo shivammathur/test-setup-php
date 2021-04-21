507f21a0a79adbc6d0b27c3120f6cff7402e007a
Shivam Mathur <shivam_jpr@hotmail.com>
2021-04-21 16:16:25 +0530

    Add rector

--- rector.php
+++ rector.php
@@ -12,7 +12,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
 
     $parameters->set(Option::PATHS, [
-        __DIR__ . '/src'
+        __DIR__ . '/vendor/typisttech/imposter'
     ]);
 
     // Define what rule sets will be applied
