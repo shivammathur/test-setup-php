6061e271b949be235c4478c9a39e8d01ce2fe099
Simon Podlipsky <simon@podlipsky.net>
2024-10-12 14:03:29 +0200

    Fix incorrect `transactional()` handling when DB auto-rolled back the transaction
    Let's get rid of There's no active transaction exception which occurs e.g. when using deferred constraints so the violation is checked at the end of the transaction and not during it.
    
    - Do not rollback only on exceptions where we know the transaction is no longer active
    - Introduce TransactionRolledBack exception
    - Transform Oracle's "transaction rolled back" exception and use the underlying one that DBAL supports

--- src/Driver/OCI8/Result.php
+++ src/Driver/OCI8/Result.php
@@ -14,6 +14,7 @@ use function oci_cancel;
 use function oci_error;
 use function oci_fetch_all;
 use function oci_fetch_array;
+use function oci_field_name;
 use function oci_num_fields;
 use function oci_num_rows;
 
