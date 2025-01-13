7d48d49e36c6bb17385c6e16f65d150758fdbe8f
Sergei Morozov <morozov@tut.by>
2025-01-13 12:26:23 -0800

    Test constraint referential action introspection

--- tests/Functional/Schema/ForeignKeyConstraintTest.php
+++ tests/Functional/Schema/ForeignKeyConstraintTest.php
@@ -5,10 +5,19 @@ declare(strict_types=1);
 namespace Doctrine\DBAL\Tests\Functional\Schema;
 
 use Doctrine\DBAL\Exception;
+use Doctrine\DBAL\Platforms\AbstractMySQLPlatform;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Platforms\DB2Platform;
+use Doctrine\DBAL\Platforms\MySQL80Platform;
+use Doctrine\DBAL\Platforms\OraclePlatform;
 use Doctrine\DBAL\Platforms\PostgreSQLPlatform;
 use Doctrine\DBAL\Platforms\SQLitePlatform;
+use Doctrine\DBAL\Platforms\SQLServerPlatform;
 use Doctrine\DBAL\Schema\Column;
 use Doctrine\DBAL\Schema\ForeignKeyConstraint;
+use Doctrine\DBAL\Schema\ForeignKeyConstraint\ReferentialAction;
+use Doctrine\DBAL\Schema\ForeignKeyConstraintEditor;
+use Doctrine\DBAL\Schema\Name\UnqualifiedName;
 use Doctrine\DBAL\Schema\Table;
 use Doctrine\DBAL\Tests\FunctionalTestCase;
 use Doctrine\DBAL\Types\Type;
@@ -53,6 +62,171 @@ final class ForeignKeyConstraintTest extends FunctionalTestCase
         $table = $sm->introspectTable('users');
 
         self::assertCount(2, $table->getForeignKeys());
+    }
+
+    /** @throws Exception */
+    #[DataProvider('referentialActionProvider')]
+    public function testOnUpdateIntrospection(ReferentialAction $action): void
+    {
+        $platform = $this->connection->getDatabasePlatform();
+
+        if (! $this->platformSupportsOnUpdateAction($platform, $action)) {
+            self::markTestSkipped(sprintf(
+                '%s does not support ON UPDATE %s',
+                $platform::class,
+                $action->value,
+            ));
+        }
+
+        $this->testReferentialActionIntrospection(
+            static function (ForeignKeyConstraintEditor $editor, ReferentialAction $action): void {
+                $editor->setOnUpdateAction($action);
+            },
+            $action,
+            static fn (ForeignKeyConstraint $constraint): ReferentialAction => $constraint->getOnUpdateAction(),
+        );
+    }
+
+    /** @throws Exception */
+    #[DataProvider('referentialActionProvider')]
+    public function testOnDeleteIntrospection(ReferentialAction $action): void
+    {
+        $platform = $this->connection->getDatabasePlatform();
+
+        if (! $this->platformSupportsOnDeleteAction($platform, $action)) {
+            self::markTestSkipped(sprintf(
+                '%s does not support ON DELETE %s',
+                $platform::class,
+                $action->value,
+            ));
+        }
+
+        $this->testReferentialActionIntrospection(
+            static function (ForeignKeyConstraintEditor $editor, ReferentialAction $action): void {
+                $editor->setOnDeleteAction($action);
+            },
+            $action,
+            static fn (ForeignKeyConstraint $constraint): ReferentialAction => $constraint->getOnDeleteAction(),
+        );
+    }
+
+    /**
+     * @param callable(ForeignKeyConstraintEditor, ReferentialAction): void $setter
+     * @param callable(ForeignKeyConstraint): ReferentialAction             $getter
+     *
+     * @throws Exception
+     */
+    private function testReferentialActionIntrospection(
+        callable $setter,
+        ReferentialAction $action,
+        callable $getter,
+    ): void {
+        $this->dropTableIfExists('users');
+        $this->dropTableIfExists('roles');
+
+        $roles = new Table('roles');
+        $roles->addColumn('id', Types::INTEGER);
+        $roles->setPrimaryKey(['id']);
+
+        $editor = ForeignKeyConstraint::editor()
+            ->setReferencingColumnNames(
+                UnqualifiedName::unquoted('role_id'),
+            )
+            ->setReferencedTableName($roles->getObjectName())
+            ->setReferencedColumnNames(
+                UnqualifiedName::unquoted('id'),
+            );
+        $setter($editor, $action);
+
+        $users = new Table('users', [
+            new Column('id', Type::getType(Types::INTEGER)),
+            new Column('role_id', Type::getType(Types::INTEGER), ['notnull' => false]),
+        ], [], [], [$editor->create()]);
+        $users->setPrimaryKey(['id']);
+
+        $sm = $this->connection->createSchemaManager();
+
+        $sm->createTable($roles);
+        $sm->createTable($users);
+
+        $constraints = $sm->listTableForeignKeys('users');
+
+        self::assertCount(1, $constraints);
+
+        $constraint = $constraints[0];
+
+        self::assertSame($action, $getter($constraint));
+    }
+
+    /** @return iterable<array{ReferentialAction}> */
+    public static function referentialActionProvider(): iterable
+    {
+        foreach (ReferentialAction::cases() as $referentialAction) {
+            yield $referentialAction->value => [$referentialAction];
+        }
+    }
+
+    private function platformSupportsOnDeleteAction(AbstractPlatform $platform, ReferentialAction $action): bool
+    {
+        return $this->platformSupportsReferentialAction($platform, $action);
+    }
+
+    private function platformSupportsOnUpdateAction(AbstractPlatform $platform, ReferentialAction $action): bool
+    {
+        if ($platform instanceof OraclePlatform) {
+            return false;
+        }
+
+        if ($platform instanceof DB2Platform) {
+            return match ($action) {
+                ReferentialAction::CASCADE,
+                ReferentialAction::SET_DEFAULT,
+                ReferentialAction::SET_NULL => false,
+                default => true,
+            };
+        }
+
+        return $this->platformSupportsReferentialAction($platform, $action);
+    }
+
+    private function platformSupportsReferentialAction(AbstractPlatform $platform, ReferentialAction $action): bool
+    {
+        if (
+            $action === ReferentialAction::RESTRICT
+            && ($platform instanceof AbstractMySQLPlatform || $platform instanceof SQLitePlatform)
+        ) {
+            self::markTestIncomplete(sprintf(
+                'Introspection of referential action %s on %s is currently unsupported',
+                $action->value,
+                $platform::class,
+            ));
+        }
+
+        if ($platform instanceof SQLServerPlatform) {
+            if ($action === ReferentialAction::RESTRICT) {
+                return false;
+            }
+        } elseif ($platform instanceof OraclePlatform) {
+            if ($action === ReferentialAction::SET_DEFAULT || $action === ReferentialAction::RESTRICT) {
+                return false;
+            }
+        } elseif ($platform instanceof DB2Platform) {
+            if ($action === ReferentialAction::SET_DEFAULT) {
+                return false;
+            }
+        } elseif (
+            $platform instanceof AbstractMySQLPlatform
+                && ! $platform instanceof MySQL80Platform
+        ) {
+            if (
+                $action === ReferentialAction::SET_DEFAULT || $action === ReferentialAction::SET_NULL
+                    || $action === ReferentialAction::CASCADE
+            ) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     /**
