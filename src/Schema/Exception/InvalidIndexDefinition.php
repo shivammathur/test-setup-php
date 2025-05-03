fc5d73710b08e64229b7b040832a68e81d6e21b2
Sergei Morozov <morozov@tut.by>
2025-05-03 08:03:11 -0700

    Include object name in editor exception messages

--- src/Schema/Exception/InvalidIndexDefinition.php
+++ src/Schema/Exception/InvalidIndexDefinition.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Doctrine\DBAL\Schema\Exception;
 
+use Doctrine\DBAL\Schema\Name\UnqualifiedName;
 use Doctrine\DBAL\Schema\SchemaException;
 use LogicException;
 
@@ -16,13 +17,17 @@ final class InvalidIndexDefinition extends LogicException implements SchemaExcep
         return new self('Index name is not set.');
     }
 
-    public static function columnsNotSet(): self
+    public static function columnsNotSet(UnqualifiedName $indexName): self
     {
-        return new self('Index column names are not set.');
+        return new self(sprintf('Columns are not set for index %s.', $indexName->toString()));
     }
 
-    public static function fromNonPositiveColumnLength(int $length): self
+    public static function fromNonPositiveColumnLength(UnqualifiedName $columnName, int $length): self
     {
-        return new self(sprintf('Indexed column length must be a positive integer, %d given.', $length));
+        return new self(sprintf(
+            'Indexed column length must be a positive integer, %d given for column %s.',
+            $length,
+            $columnName->toString(),
+        ));
     }
 }
