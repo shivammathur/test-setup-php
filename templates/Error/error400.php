e19e553b594287507375fab1239080f26a810ace
Shivam Mathur <shivam_jpr@hotmail.com>
2026-03-21 17:46:52 +0530

    Add CakePHP MySQL example branch

---
+++ templates/Error/error400.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @var \App\View\AppView $this
+ * @var string $message
+ * @var string $url
+ */
+use Cake\Core\Configure;
+
+$this->setLayout('error');
+
+if (Configure::read('debug')) :
+    $this->setLayout('dev_error');
+
+    $this->assign('title', $message);
+    $this->assign('templateName', 'error400.php');
+
+    $this->start('file');
+    echo $this->element('auto_table_warning');
+    $this->end();
+endif;
+?>
+<h2><?= h($message) ?></h2>
+<p class="error">
+    <strong><?= __d('cake', 'Error') ?>: </strong>
+    <?= __d('cake', 'The requested address {0} was not found on this server.', "<strong>'{$url}'</strong>") ?>
+</p>
---
+++ templates/Error/error500.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @var \App\View\AppView $this
+ * @var string $message
+ * @var string $url
+ */
+use Cake\Core\Configure;
+use Cake\Error\Debugger;
+
+$this->setLayout('error');
+
+if (Configure::read('debug')) :
+    $this->setLayout('dev_error');
+
+    $this->assign('title', $message);
+    $this->assign('templateName', 'error500.php');
+
+    $this->start('file');
+?>
+<?php if ($error instanceof Error) : ?>
+    <?php $file = $error->getFile() ?>
+    <?php $line = $error->getLine() ?>
+    <strong>Error in: </strong>
+    <?= $this->Html->link(sprintf('%s, line %s', Debugger::trimPath($file), $line), Debugger::editorUrl($file, $line)); ?>
+<?php endif; ?>
+<?php
+    echo $this->element('auto_table_warning');
+
+    $this->end();
+endif;
+?>
+<h2><?= __d('cake', 'An Internal Error Has Occurred.') ?></h2>
+<p class="error">
+    <strong><?= __d('cake', 'Error') ?>: </strong>
+    <?= h($message) ?>
+</p>
