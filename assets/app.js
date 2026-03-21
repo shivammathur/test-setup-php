cdd6ca7912a5c1b47d00199039d247ca202e2745
Shivam Mathur <shivam_jpr@hotmail.com>
2026-03-21 16:02:43 +0530

    Use Symfony demo scaffold for Postgres example

---
+++ assets/app.js
@@ -0,0 +1,17 @@
+// start the Stimulus application
+import './stimulus_bootstrap.js';
+import './styles/app.scss';
+import 'highlight.js/styles/github-dark-dimmed.css';
+import 'lato-font/css/lato-font.css';
+
+// loads the Bootstrap plugins
+import 'bootstrap';
+
+// loads the code syntax highlighting library
+import './js/highlight.js';
+
+// Creates links to the Symfony documentation
+import './js/doclinks.js';
+
+import './js/flatpicker.js';
+
