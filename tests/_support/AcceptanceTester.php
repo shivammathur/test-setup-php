b28f8df292b1038c3e97a3224b29411d789ec3c9
Shivam Mathur <shivam_jpr@hotmail.com>
2026-03-21 16:17:27 +0530

    Test phalcon postgres example

--- tests/_support/AcceptanceTester.php
+++ tests/_support/AcceptanceTester.php
@@ -1,13 +1,10 @@
 <?php
 
-declare(strict_types=1);
-
-namespace Tests;
 
 /**
  * Inherited Methods
- * @method void wantTo($text)
  * @method void wantToTest($text)
+ * @method void wantTo($text)
  * @method void execute($callable)
  * @method void expectTo($prediction)
  * @method void expect($prediction)
@@ -15,7 +12,7 @@ namespace Tests;
  * @method void am($role)
  * @method void lookForwardTo($achieveValue)
  * @method void comment($description)
- * @method void pause($vars = [])
+ * @method void pause()
  *
  * @SuppressWarnings(PHPMD)
 */
@@ -23,7 +20,7 @@ class AcceptanceTester extends \Codeception\Actor
 {
     use _generated\AcceptanceTesterActions;
 
-    /**
-     * Define custom actions here
-     */
+   /**
+    * Define custom actions here
+    */
 }
