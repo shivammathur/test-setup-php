2300a0737447e5c7cdb8259ad845439b49aa4e23
Shivam Mathur <shivam_jpr@hotmail.com>
2026-03-21 16:04:20 +0530

    Add Yii2 app scaffold

---
+++ common/config/test.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'id' => 'app-common-tests',
+    'basePath' => dirname(__DIR__),
+    'components' => [
+        'user' => [
+            'class' => \yii\web\User::class,
+            'identityClass' => 'common\models\User',
+        ],
+    ],
+];
